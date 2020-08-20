fdb018f023ec5bfec04d97d97a45b9d96ec8001a
manjeetk <techmanjeet2006@gmail.com>
2020-08-20 17:39:13 +0530

    spring boot application

---
+++ springboot-in-10-steps/src/main/java/com/in24minutes/springboot/basics/springbootin10steps/BookController.java
@@ -0,0 +1,21 @@
+package com.in24minutes.springboot.basics.springbootin10steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+	@GetMapping("/books")
+	public List<Book> getBooks() {
+		
+		
+		return Arrays.asList(
+				new Book(1L,"Java","sriman"));
+		
+	}
+	
+}
